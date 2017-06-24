a19a6a8562a27ae7155fb7418413cf4bcf32eedb
Simon Parry <simon.parry@coshx.co.uk>
2017-06-24 21:10:20 +0100

    had a go at bigstep style I think

--- src/Main.ml
+++ src/Main.ml
@@ -4,8 +4,11 @@ open UntypedArith
 let test = 
   let z = (TmZero dummyinfo) in
   let s = (TmSucc (dummyinfo, z)) in
-  let ex = eval s in 
-  print_string (string_of_bool (isnumerical ex))
+  let ex1 = eval s in 
+  let ex2 = eval_bigstep s in 
+  let s1 =(string_of_bool (isnumerical ex1)) in
+  let s2 = (string_of_bool (isnumerical ex2)) in
+  print_string (s1 ^ ", " ^ s2)
 
 let () =
   test ;
--- src/UntypedArith.ml
+++ src/UntypedArith.ml
@@ -61,4 +61,35 @@ let rec eval t =
   try let t' = eval1 t
       in eval t'
   with NoRuleApplies -> t
+;;
 
+(* TODO big step style *)
+let rec eval2 t = match t with
+  TmIf (_, TmTrue (_), t2, t3) -> eval2 t2
+| TmIf (_, TmFalse (_), t2, t3) -> eval2 t3
+| TmIf (fi, t1, t2, t3) ->
+    let t1' = eval2 t1 in
+    TmIf (fi, t1', t2, t3)
+| TmSucc (fi, t1) ->
+    let t1' = eval2 t1 in
+    TmSucc (fi, t1')
+| TmPred (_, TmZero (_)) -> TmZero dummyinfo
+| TmPred (_, TmSucc ((_, nv))) when (isnumerical nv) -> eval2 nv
+| TmPred (fi, t1) ->
+    let t1' = eval2 t1 in
+    TmPred (fi, t1')
+| TmIsZero (_, (TmZero _)) ->
+    TmTrue dummyinfo
+| TmIsZero (_, (TmSucc (_, nv))) when (isnumerical nv) ->
+    TmFalse dummyinfo
+| TmIsZero (fi, t1) ->
+  let t1' = eval2 t1 in
+  TmIsZero (fi, t1')
+| _ -> raise NoRuleApplies
+
+
+let rec eval_bigstep t =
+  try let t' = eval2 t
+      in eval_bigstep t'
+  with NoRuleApplies -> t
+;;
