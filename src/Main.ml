3cb962f0be992684bd02ea017677d762cdd4380d
Simon Parry <simon.parry@coshx.co.uk>
2017-06-24 09:14:55 +0100

    little harness

--- src/Main.ml
+++ src/Main.ml
@@ -1,2 +1,13 @@
-print_string "hello world\n"
+open UntypedArith
+;;
+
+let test = 
+  let z = (TmZero dummyinfo) in
+  let s = (TmSucc (dummyinfo, z)) in
+  let ex = eval s in 
+  print_string (string_of_bool (isnumerical ex))
+
+let () =
+  test ;
+  print_string "\ndone\n"
              
